c0547fb43fa90ca168071b2c49eedb7b93fd919f
Edwin John Flores <edwin.flores@klab.com>
2014-11-28 11:35:21 +0800

    Merged master and organize1.0

--- cSlashesTest.php
+++
@@ -1,7 +0,0 @@
-<?php
-	echo addcslashes("hello\tworld\n", "\x00..\x1fz..\xff") . "\n".
-
-	$string = stripcslashes('hello\tworld\n');
-
-	echo "$string \n";
-?>
--- queryStringTest.php
+++
@@ -1,6 +0,0 @@
-<?php
-	$baseUrl = 'http://www.google.com/q=';
-	$query = 'Why did the chicken cross the road -cookies';
-	$url = $baseUrl . urlencode($query);
-	echo $url . "\n";
-?>
